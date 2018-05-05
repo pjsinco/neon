7f093d4a636c34d0fe5c7c5ae3c5d3b45ac552f9
pjsinco <psinco@gmail.com>
2018-05-05 08:54:30 -0500

    Fix projectile starting position

--- src/Ship.lua
+++ src/Ship.lua
@@ -10,10 +10,12 @@ function Ship:init(params)
 end
 
 function Ship:update(dt)
+    Entity.update(self, dt)
+
     if love.keyboard.wasPressed('space') then
         table.insert(self.projectiles, Projectile({
             x = self.x + self.width,
-            y = self.y / 2,
+            y = self.y + (self.height / 2),
             speed = ENTITY_DEFS['projectile-1'].speed,
             texture = ENTITY_DEFS['projectile-1'].texture,
             animations = ENTITY_DEFS['projectile-1'].animations,
@@ -24,8 +26,6 @@ function Ship:update(dt)
     for _, projectile in pairs(self.projectiles) do
         projectile:update(dt)
     end
-
-    Entity.update(self, dt)
 end
 
 function Ship:render()
